276d8a62689f0540909dd141a45209656b65a593
Oyun-Erdene Adilbish <64121596+oyuka1112@users.noreply.github.com>
2021-11-05 15:26:16 -0400

    Adding the election audit code

--- PyPoll.py
+++ PyPoll.py
@@ -5,18 +5,13 @@ import os
 file_to_load = os.path.join("election_results.csv")
 # Assign a variable to save the file to a path.
 file_to_save = os.path.join("election_analysis.txt")
-my_list = list()
-# def unique(list1):
- 
-#     # initialize a null list
-#     unique_list = []
-     
-#     # traverse for all elements
-#     for x in list1:
-#         # check if exists in unique_list or not
-#         if x not in unique_list:
-#             unique_list.append(x)
-  
+
+total_votes = 0 
+candidate_options = []
+candidate_votes ={}
+winning_candidate = ""
+winning_count = 0
+winning_percentage = 0
 # Open the election results and read the file.
 with open(file_to_load) as election_data:
     file_reader = csv.reader(election_data)
@@ -24,11 +19,37 @@ with open(file_to_load) as election_data:
     # Read and print the header row.
     headers = next(file_reader)
     print(headers)
+   
     for row in file_reader:
-        my_list.append(row[2])
+        total_votes+=1
+        candidate_name = row[2]
+        if candidate_name not in candidate_options:
+            candidate_options.append(candidate_name)
+            candidate_votes[candidate_name] = 0
+        candidate_votes[candidate_name] += 1
         
+    for candidate_name in candidate_votes:
+        votes = candidate_votes[candidate_name]
+        vote_percentage = float(votes)/float(total_votes) * 100
+        print(f"{candidate_name}: {vote_percentage:.1f}% ({votes:,})\n")
+        if (votes > winning_count) and (vote_percentage > winning_percentage):
+            winning_count = votes
+            winning_percentage = vote_percentage
+            winning_candidate = candidate_name
+        winning_candidate_summary = (
+            f"-------------------------\n"
+            f"Winner: {winning_candidate}\n"
+            f"Winning Vote Count: {winning_count:,}\n"
+            f"Winning Percentage: {winning_percentage:.1f}%\n"
+            f"-------------------------\n")
+    print(winning_candidate_summary)
+
+# print(total_votes)    
+# print(candidate_options)
+# print(candidate_votes)
+# print(winning_candidate)
  
-print(unique(my_list))
+
 
 
 # 1. total num of votes cast
@@ -36,8 +57,3 @@ print(unique(my_list))
 # 3. The percentage of votes each candidate won
 # 4. The total number of votes each candadates won
 # 5. The winner of the election based on popular vote
-
-
-import datetime as dt
-now = dt.datetime.now()
-print(now)
